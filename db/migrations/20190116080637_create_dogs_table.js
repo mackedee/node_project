ffd6bd3ffa87a2cfaa51ddf49892e193c3423091
Mack Skodiak <mackedee@gmail.com>
2019-01-23 13:44:05 -0700

    still don't know where this is writing to

---
+++ db/migrations/20190116080637_create_dogs_table.js
@@ -0,0 +1,13 @@
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('users', (table) => {
+    table.increments();
+    table.string('breed').notNullable();
+    table.string('size').notNullable();
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('dogs')
+};
