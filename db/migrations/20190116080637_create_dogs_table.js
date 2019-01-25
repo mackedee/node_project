9a41d4253ae101e69f397ab72ef0aa209e1f74f1
Mack Skodiak <mackedee@gmail.com>
2019-01-25 09:07:34 -0700

    fix table name

--- db/migrations/20190116080637_create_dogs_table.js
+++ db/migrations/20190116080637_create_dogs_table.js
@@ -1,5 +1,5 @@
 exports.up = function(knex, Promise) {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('dogs', (table) => {
     table.increments();
     table.string('breed').notNullable();
     table.string('size').notNullable();
